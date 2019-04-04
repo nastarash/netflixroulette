3551e8294e613439fc50aa2f37f2bbfac97def64
Anastasiya <a.rashinskaya@gmail.com>
2019-04-04 21:34:36 +0300

    Add some styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
+    <header
+      className="site-header size-lg text-center"
+      style={{ backgroundImage: 'url(../../../public/Netflix-1.jpg)' }}
+    >
       <h1>netflixroullete</h1>
     </header>
   );
